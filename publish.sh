1877050555f477d559dfcd5b8d2ab7406b6fd7af
lihuichao <lihuichaolihc@gmail.com>
2020-03-01 15:26:37 +0800

    fix publish.md

--- publish.sh
+++ publish.sh
@@ -12,17 +12,9 @@ read -p "Are you sure commit " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-
-    function myfunc()
-    {
-        printf "\'%s\'" "$1"
-    }
-    # define function which quote the command arg
-    message=$(myfunc "$1")
-    echo $message
     git add .
     echo 'git add successfully!'
-    git commit -m $message
+    git commit -m "$1"
     echo 'git commit successfully'
     git push
 fi
